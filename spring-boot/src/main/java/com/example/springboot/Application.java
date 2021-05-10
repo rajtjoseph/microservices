a0eef0dd386954171bebaa36945ae8b501226911
rajtjoseph <rajtjoseph@gmail.com>
2021-05-10 16:50:28 -0500

    Update Application.java

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -13,6 +13,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("This is a Spring Boot ");
 	}
 	
 /*
