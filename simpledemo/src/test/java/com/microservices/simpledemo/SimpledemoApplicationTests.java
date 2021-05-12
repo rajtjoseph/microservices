b39fcaae63f63ac3c99f61fc798e063c4d91680b
rajtjoseph <rajtjoseph@gmail.com>
2021-05-11 20:17:29 -0500

    Add files via upload

---
+++ simpledemo/src/test/java/com/microservices/simpledemo/SimpledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.simpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
