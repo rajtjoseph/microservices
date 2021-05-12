8a99498e0fd03fbea229a9cc48c415026ee6be3a
rajtjoseph <rajtjoseph@gmail.com>
2021-05-11 20:18:23 -0500

    Update SimpledemoApplication.java

--- simpledemo/src/main/java/com/microservices/simpledemo/SimpledemoApplication.java
+++ simpledemo/src/main/java/com/microservices/simpledemo/SimpledemoApplication.java
@@ -3,6 +3,12 @@ package com.microservices.simpledemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+* @author Raj Joseph
+* @version 1.0
+*
+**/
 @SpringBootApplication
 public class SimpledemoApplication {
 
