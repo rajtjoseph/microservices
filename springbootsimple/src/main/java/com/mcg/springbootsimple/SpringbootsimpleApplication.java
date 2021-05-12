05c00c0004ec09fcac7ee52318e9b369354451e2
rajtjoseph <rajtjoseph@gmail.com>
2021-05-12 13:46:25 -0500

    Add files via upload

---
+++ springbootsimple/src/main/java/com/mcg/springbootsimple/SpringbootsimpleApplication.java
@@ -0,0 +1,13 @@
+package com.mcg.springbootsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsimpleApplication.class, args);
+	}
+
+}
---
+++ springbootsimple/src/test/java/com/mcg/springbootsimple/SpringbootsimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcg.springbootsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
