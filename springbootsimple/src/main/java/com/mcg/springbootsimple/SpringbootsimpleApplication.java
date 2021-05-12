3bc9b5c9396a9ec89be2c4bcaa838a13f76e29c9
rajtjoseph <rajtjoseph@gmail.com>
2021-05-12 13:49:29 -0500

    Update SpringbootsimpleApplication.java
    Added sysout debugging code

--- springbootsimple/src/main/java/com/mcg/springbootsimple/SpringbootsimpleApplication.java
+++ springbootsimple/src/main/java/com/mcg/springbootsimple/SpringbootsimpleApplication.java
@@ -2,12 +2,17 @@ package com.mcg.springbootsimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+* @author Raj 
+* @version 1.0
+*
+**/
 @SpringBootApplication
 public class SpringbootsimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootsimpleApplication.class, args);
+		System.out.println("Simple Spring Boot ");
 	}
 
 }
